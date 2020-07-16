537a7e9117389bac31aaa128e592b23b2eab11d2
Massimo Costa <massimoc@filewave.com>
2020-07-16 16:31:11 +0200

    feat: use postgres as database engine

--- docker/db/init_scripts/0001_create_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER admin LOGIN PASSWORD 'admin';
-    CREATE DATABASE app;
-    GRANT ALL PRIVILEGES ON DATABASE app TO admin;
-EOSQL
--- project/books/models.py
+++ project/books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+from django.contrib.postgres.fields.citext import CITextField
+
+
 # Create your models here.
+
+class Author(models.Model):
+    first_name = CITextField()
+    last_name = CITextField()
+    date_of_birth = models.DateField(null=True)
+
+
+class Book(models.Model):
+    title = CITextField(null=False)
+    author = models.ForeignKey(
+        Author,
+        models.CASCADE,
+        related_name='books'
+    )
+    copies_sold = models.PositiveIntegerField(
+        default=0,
+    )
